b6359c9477001e4564ead4e86636d5dede1148e0
Chris Sägner <chris.saegner99@web.de>
2022-11-23 22:35:41 +0100

    add frontend container + edit Folder structure

---
+++ backend/app/Http/Controllers/GuestbookController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Guestbook;
+use Illuminate\Http\Request;
+
+class GuestbookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $guestbook = Guestbook::latest()->paginate(10);
+    
+        return response()->json($guestbook);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'title' => 'required',
+            'text' => 'required',
+        ]);
+    
+        $guestbook = Guestbook::create($request->all());
+     
+        return response()->json($guestbook);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Guestbook  $guestbook
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Guestbook $guestbook)
+    {
+        // TODO: maybe get the guestbook entry by id and return back all information
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Guestbook  $guestbook
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Guestbook $guestbook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Guestbook  $guestbook
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Guestbook $guestbook)
+    {
+        $request->validate([
+            'name' => 'required',
+            'title' => 'required',
+            'text' => 'required',
+        ]);
+    
+        $guestbook->update($request->all());
+    
+        return response()->json($guestbook);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Guestbook  $guestbook
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Guestbook $guestbook)
+    {
+        $guestbook->delete();
+    
+        return response(null, 204);
+    }
+}
---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::latest()->paginate(5);
+    
+        return response()->json($users);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() { }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+    
+        $user = User::create($request->all());
+     
+        return response()->json($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user) 
+    {
+        // TODO: maybe get the user by mail and return back all information
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user) { }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+    
+        $user->update($request->all());
+    
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+    
+        return response(null, 204);
+    }
+}
