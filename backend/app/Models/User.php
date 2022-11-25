a2f01bc836e26598eb04c4216d68f8f9a41e8652
Chris Sägner <chris.saegner99@web.de>
2022-11-25 16:22:00 +0100

    function to set the password attribut

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -41,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+        $this->attributes['remember_token'] = Str::random(10);
+    }
 }
