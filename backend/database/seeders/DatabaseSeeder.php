75873de3f2350dad3a9876a92f40f3a9ee85a56f
Chris Sägner <chris.saegner99@web.de>
2022-11-25 16:22:23 +0100

    change from 10 to 100

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\Guestbook::factory(10)->create();
+        \App\Models\Guestbook::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
